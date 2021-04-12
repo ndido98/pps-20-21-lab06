cb190c2d847c6d0f88ccc543c2826b2c8060fd3c
ndido98 <ndido98@gmail.com>
2021-04-12 12:07:57 +0200

    Add NotTwoConsecutive mixin

--- src/u06lab/code/Parser.scala
+++ src/u06lab/code/Parser.scala
@@ -12,9 +12,7 @@ package u06lab.code
 abstract class Parser[T] {
     def parse(t: T): Boolean // is the token accepted?
     def end(): Boolean // is it ok to end here
-    def parseAll(seq: Seq[T]): Boolean = (seq forall {
-        parse(_)
-    }) & end() // note &, not &&
+    def parseAll(seq: Seq[T]): Boolean = (seq forall parse) & end() // note &, not &&
 }
 
 class BasicParser(chars: Set[Char]) extends Parser[Char] {
@@ -26,55 +24,31 @@ class BasicParser(chars: Set[Char]) extends Parser[Char] {
 trait NonEmpty[T] extends Parser[T] {
     private[this] var empty = true
 
-    abstract override def parse(t: T) = {
-        empty = false; super.parse(t)
-    } // who is super??
-    abstract override def end() = !empty && {
-        empty = true; super.end()
+    abstract override def parse(t: T): Boolean = {
+        empty = false
+        super.parse(t)
+    }
+    abstract override def end(): Boolean = !empty && {
+        empty = true
+        super.end()
     }
 }
 
 class NonEmptyParser(chars: Set[Char]) extends BasicParser(chars) with NonEmpty[Char]
 
 trait NotTwoConsecutive[T] extends Parser[T] {
-    // ???
+    private[this] var lastElem: Option[T] = None
+
+    abstract override def parse(t: T): Boolean = {
+        val res = lastElem match {
+            case Some(value) if t == value => false
+            case _ => super.parse(t)
+        }
+        lastElem = Some(t)
+        res
+    }
+
+    abstract override def end(): Boolean = super.end()
 }
 
-class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) // ??? with ...
-
-
-object TryParsers extends App {
-    def parser = new BasicParser(Set('a', 'b', 'c'))
-
-    println(parser.parseAll("aabc".toList)) // true
-    println(parser.parseAll("aabcdc".toList)) // false
-    println(parser.parseAll("".toList)) // true
-
-    // Note NonEmpty being "stacked" on to a concrete class
-    // Bottom-up decorations: NonEmptyParser -> NonEmpty -> BasicParser -> Parser
-    def parserNE = new NonEmptyParser(Set('0', '1'))
-
-    println(parserNE.parseAll("0101".toList)) // true
-    println(parserNE.parseAll("0123".toList)) // false
-    println(parserNE.parseAll(List())) // false
-
-    def parserNTC = new NotTwoConsecutiveParser(Set('X', 'Y', 'Z'))
-
-    println(parserNTC.parseAll("XYZ".toList)) // true
-    println(parserNTC.parseAll("XYYZ".toList)) // false
-    println(parserNTC.parseAll("".toList)) // true
-
-    // note we do not need a class name here, we use the structural type
-    def parserNTCNE = new BasicParser(Set('X', 'Y', 'Z')) with NotTwoConsecutive[Char] with NonEmpty[Char]
-
-    println(parserNTCNE.parseAll("XYZ".toList)) // true
-    println(parserNTCNE.parseAll("XYYZ".toList)) // false
-    println(parserNTCNE.parseAll("".toList)) // false
-
-    def sparser: Parser[Char] = ??? // "abc".charParser()
-    println(sparser.parseAll("aabc".toList)) // true
-    println(sparser.parseAll("aabcdc".toList)) // false
-    println(sparser.parseAll("".toList)) // true
-}
-
-
+class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) with NotTwoConsecutive[Char]
