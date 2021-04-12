7712fe04b68500320dbee87dc2ccea4413e05981
ndido98 <ndido98@gmail.com>
2021-04-12 11:39:38 +0200

    Reformat project

--- test/u06lab/code/Parser.scala
+++ test/u06lab/code/Parser.scala
@@ -4,49 +4,53 @@ import org.junit.jupiter.api.{Assertions, Test}
 import Assertions._
 
 class TryParsers {
-  def parser = new BasicParser(Set('a', 'b', 'c'))
-  // Note NonEmpty being "stacked" on to a concrete class
-  // Bottom-up decorations: NonEmptyParser -> NonEmpty -> BasicParser -> Parser
-  def parserNE = new NonEmptyParser(Set('0', '1'))
-  def parserNTC = new NotTwoConsecutiveParser(Set('X', 'Y', 'Z'))
-  // note we do not need a class name here, we use the structural type
-  def parserNTCNE = new BasicParser(Set('X', 'Y', 'Z')) with NotTwoConsecutive[Char] with NonEmpty[Char]
-  def sparser: Parser[Char] = ??? // "abc".charParser()
+    def parser = new BasicParser(Set('a', 'b', 'c'))
 
-  @Test
-  def testBasicParser = {
-    assertTrue(parser.parseAll("aabc".toList))
-    assertFalse(parser.parseAll("aabcdc".toList))
-    assertTrue(parser.parseAll("".toList))
-  }
+    // Note NonEmpty being "stacked" on to a concrete class
+    // Bottom-up decorations: NonEmptyParser -> NonEmpty -> BasicParser -> Parser
+    def parserNE = new NonEmptyParser(Set('0', '1'))
 
-  @Test
-  def testNotEmptyParser = {
-    assertTrue(parserNE.parseAll("0101".toList))
-    assertFalse(parserNE.parseAll("0123".toList))
-    assertFalse(parserNE.parseAll(List()))
-  }
+    def parserNTC = new NotTwoConsecutiveParser(Set('X', 'Y', 'Z'))
 
-  @Test
-  def testNotTwoConsecutiveParser = {
-    assertTrue(parserNTC.parseAll("XYZ".toList))
-    assertFalse(parserNTC.parseAll("XYYZ".toList))
-    assertTrue(parserNTC.parseAll("".toList))
-  }
+    // note we do not need a class name here, we use the structural type
+    def parserNTCNE = new BasicParser(Set('X', 'Y', 'Z')) with NotTwoConsecutive[Char] with NonEmpty[Char]
 
-  @Test
-  def testNotEmptyAndNotTwoConsecutiveParser = {
-    assertTrue(parserNTCNE.parseAll("XYZ".toList))
-    assertFalse(parserNTCNE.parseAll("XYYZ".toList))
-    assertFalse(parserNTCNE.parseAll("".toList))
-  }
+    def sparser: Parser[Char] = ??? // "abc".charParser()
 
-  @Test
-  def testStringParser = {
-    assertTrue(sparser.parseAll("aabc".toList))
-    assertFalse(sparser.parseAll("aabcdc".toList))
-    assertTrue(sparser.parseAll("".toList))
-  }
+    @Test
+    def testBasicParser = {
+        assertTrue(parser.parseAll("aabc".toList))
+        assertFalse(parser.parseAll("aabcdc".toList))
+        assertTrue(parser.parseAll("".toList))
+    }
+
+    @Test
+    def testNotEmptyParser = {
+        assertTrue(parserNE.parseAll("0101".toList))
+        assertFalse(parserNE.parseAll("0123".toList))
+        assertFalse(parserNE.parseAll(List()))
+    }
+
+    @Test
+    def testNotTwoConsecutiveParser = {
+        assertTrue(parserNTC.parseAll("XYZ".toList))
+        assertFalse(parserNTC.parseAll("XYYZ".toList))
+        assertTrue(parserNTC.parseAll("".toList))
+    }
+
+    @Test
+    def testNotEmptyAndNotTwoConsecutiveParser = {
+        assertTrue(parserNTCNE.parseAll("XYZ".toList))
+        assertFalse(parserNTCNE.parseAll("XYYZ".toList))
+        assertFalse(parserNTCNE.parseAll("".toList))
+    }
+
+    @Test
+    def testStringParser = {
+        assertTrue(sparser.parseAll("aabc".toList))
+        assertFalse(sparser.parseAll("aabcdc".toList))
+        assertTrue(sparser.parseAll("".toList))
+    }
 }
 
 
